faf84de09c3ef223d779ff3358336f84d9510c6f
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2025-07-18 15:09:31 -0500

    Add new fav and updated crowd fav data file

--- client/src/data/crowdFavorites.js
+++ client/src/data/crowdFavorites.js
@@ -1,1 +1,51 @@
-import image1 from 
+import favimg1 from '../assets/img/crowd-fav-1.png';
+import favimg2 from '../assets/img/crowd-fav-2.png';
+import favimg3 from '../assets/img/crowd-fav-3.png';
+import favimg4 from '../assets/img/crowd-fav-4.png';  
+
+const crowdFavorites = [
+  {
+    id: 1,
+    name: 'Deep-Dish Seasonal Cobbler',
+    description:'Seasonal fruit topped with a sweet caramelized cake topping, vanilla bean ice cream and a drizzle of caramel',
+    category: 'Dessert',
+    price: "$8.99",
+    image: favimg1,
+    tags: ['Gluten-Free', 'Popular'],
+  },
+  {
+    id: 2,
+    name: 'Cuban-Style Pulled Pork',
+    description: 'Slow-cooked pulled pork with a tangy citrus marinade, served with pickled onions and cilantro',
+    category: 'Sandwich',
+    price: "$14.99",
+    image: favimg2,
+    tags: ['Popular', 'Spicy'],
+  },
+  {
+    id: 3,
+    name: 'Nashville Hot Chicken Sandwich',
+    description: 'Spicy fried chicken breast, pickles, and slaw on a brioche bun',
+    category: 'Sandwich',
+    price: "$12.99",
+    image: favimg3,
+    tags: ['Spicy', 'Popular'],
+  },
+  {
+    id: 4,
+    name: 'Shrimp Po Boy Sandwich',
+    description: 'Crispy shrimp, lettuce, tomato, and remoulade sauce on a toasted hoagie roll',
+    category: 'Sandwich',
+    price: "$16.99",
+    image: favimg4,
+    tags: ['Gluten-Free', 'Popular'],
+  },
+  {
+    id : 5,
+    name: 'BBQ Brisket Sandwich',
+    description: 'Tender brisket smothered in tangy BBQ sauce, topped with coleslaw on a toasted bun',
+    category: 'Sandwich',
+    price: "$15.99",
+    image: favimg5,
+    tags: ['Gluten-Free', 'Popular'],
+  }
